f39881248b1ec6ea072124d54c5bc2cb57422b8f
Petr Pondělík <pondepe1@fit.cvut.cz>
2020-11-29 16:03:31 +0100

    Apply more precise centralDegree formule in Greate Circle Distance.

--- router/photos.ts
+++ router/photos.ts
@@ -10,8 +10,12 @@ let apiClient: UnsplashApiClient = new UnsplashApiClient();
 /** GET photos */
 router.post('/search', (req, res, next) => {
 
+    var startTime = new Date();
+
     apiClient.searchPhotos(req.body['query'], req.body['amount']).then(
         (result: ResponseInterface) => {
+            let apiEndTime = new Date();
+            console.log(apiEndTime.getTime() - startTime.getTime());
             let similarity: Similarity = new Similarity(result.results.length);
             /** If there is location in query, get photos details from Unsplash API */
             let photos: Array<PhotoInterface> = result.results;
@@ -27,6 +31,8 @@ router.post('/search', (req, res, next) => {
                 });
             } else {
                 let photosReRanked: Array<PhotoInterface> = similarity.reRank(photos, req.body);
+                let totalEndTime = new Date();
+                console.log(totalEndTime.getTime() - startTime.getTime());
                 res.send({
                     "res": {
                         "original": result.results,
